ad8e050b4c669dc2fe7c36fa0a3c05e621aead1c
Panuinc <panuwat.ja.forwork@gmail.com>
2025-07-16 16:42:50 +0700

    update

---
+++ app/pages/human/layout.jsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function HumanLayout({ children }) {
+  return <>1</>;
+}
---
+++ app/pages/human/page.jsx
@@ -0,0 +1,10 @@
+import UIHuman from "@/modules/human/UIHuman";
+import React from "react";
+
+export default function human() {
+  return (
+    <>
+      <UIHuman />
+    </>
+  );
+}
