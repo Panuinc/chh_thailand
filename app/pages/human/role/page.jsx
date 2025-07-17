a6153a97f4be8bc55929dd2a203845a3c8a978ae
Panuinc <panuwat.ja.forwork@gmail.com>
2025-07-17 09:34:31 +0700

    update

---
+++ app/pages/human/role/page.jsx
@@ -0,0 +1,10 @@
+import UIRoleList from "@/modules/human/role/components/UIRoleList";
+import React from "react";
+
+export default function roleList() {
+  return (
+    <>
+      <UIRoleList headerContent="Role List" />
+    </>
+  );
+}
---
+++ modules/human/role/components/UIRoleList.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import UITopic from "@/components/topic/UITopic";
+
+export default function UIRoleList({ headerContent }) {
+  return (
+    <>
+      <UITopic Topic={headerContent} />
+      <div className="flex flex-col lg:flex-row items-center justify-center w-full p-2 gap-2 border-2 border-dark border-dashed rounded-lg">
+        <div className="flex items-center justify-center w-full h-40 p-2 gap-2 border-2 border-dark border-dashed">
+          1
+        </div>
+        <div className="flex items-center justify-center w-full h-40 p-2 gap-2 border-2 border-dark border-dashed">
+          1
+        </div>
+        <div className="flex items-center justify-center w-full h-40 p-2 gap-2 border-2 border-dark border-dashed">
+          1
+        </div>
+      </div>
+      <div className="flex flex-col lg:flex-row items-center justify-center w-full p-2 gap-2 border-2 border-dark border-dashed rounded-lg">
+        <div className="flex items-center justify-center w-full h-40 p-2 gap-2 border-2 border-dark border-dashed">
+          1
+        </div>
+        <div className="flex items-center justify-center w-full h-40 p-2 gap-2 border-2 border-dark border-dashed">
+          1
+        </div>
+        <div className="flex items-center justify-center w-full h-40 p-2 gap-2 border-2 border-dark border-dashed">
+          1
+        </div>
+      </div>
+      <div className="flex flex-col lg:flex-row items-center justify-center w-full p-2 gap-2 border-2 border-dark border-dashed rounded-lg">
+        <div className="flex items-center justify-center w-full h-40 p-2 gap-2 border-2 border-dark border-dashed">
+          1
+        </div>
+        <div className="flex items-center justify-center w-full h-40 p-2 gap-2 border-2 border-dark border-dashed">
+          1
+        </div>
+        <div className="flex items-center justify-center w-full h-40 p-2 gap-2 border-2 border-dark border-dashed">
+          1
+        </div>
+      </div>
+    </>
+  );
+}
