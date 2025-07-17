10790517909585e1b7e10005b01d8269719e5153
Panuinc <panuwat.ja.forwork@gmail.com>
2025-07-17 08:18:28 +0700

    updat

--- app/pages/human/layout.jsx
+++ app/pages/human/layout.jsx
@@ -1,5 +1,9 @@
 "use client";
 
 export default function HumanLayout({ children }) {
-  return <>1</>;
+  return (
+    <>
+      <>1</>
+    </>
+  );
 }
--- modules/human/UIHuman.jsx
+++ modules/human/UIHuman.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function UIHuman() {
-  return (
-    <div>
-      UIHuman
-    </div>
-  )
+  return <>UIHuman</>;
 }
