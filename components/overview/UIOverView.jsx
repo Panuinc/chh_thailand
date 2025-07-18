1562572c7e7d53accfd6c5cefa3088080c6f4c99
Panuinc <panuwat.ja.forwork@gmail.com>
2025-07-18 08:42:26 +0700

    update

--- components/overview/UIOverView.jsx
+++ components/overview/UIOverView.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import UITopic from "@/components/topic/UITopic";
+import Image from "next/image";
 
 export default function UIOverView({ headerContent }) {
   return (
     <>
       <div className="flex flex-col lg:flex-row items-center justify-start w-full h-full p-2 gap-2 border-2 border-dark border-dashed rounded-2xl overflow-auto">
-        <div className="lg:flex hidden flex-col items-center justify-center w-full lg:w-3/12 h-full p-2 gap-2 border-2 border-dark border-dashed rounded-xl">
-          1
+        <div className="lg:flex hidden flex-col items-center justify-center w-full lg:w-3/12 h-full p-2 gap-2 border-2 border-dark border-dashed rounded-xl relative">
+          <Image
+            src="/picture/dashboard.jpg"
+            alt="dashboard"
+            fill
+            style={{ objectFit: "cover", borderRadius: "0.75rem" }}
+            priority
+          />
         </div>
         <div className="flex flex-col items-center justify-start w-full lg:w-9/12 h-full p-2 gap-2 border-2 border-dark border-dashed rounded-xl">
           <UITopic Topic={headerContent} />
