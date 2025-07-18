9af94b0ce9aa43f36b87385d18a0f124b839b0a9
Panuinc <panuwat.ja.forwork@gmail.com>
2025-07-18 09:04:42 +0700

    update

---
+++ app/api/human/user/[userId]/route.js
@@ -0,0 +1,13 @@
+import { getUserById, updateUser } from "@/modules/human/user/controllers/user.controller";
+
+export async function GET(request, context) {
+  const { userId } = await context.params;
+  return getUserById(request, userId);
+}
+
+export async function PUT(request, context) {
+  const { userId } = await context.params;
+  return updateUser(request, userId);
+}
+
+export const dynamic = "force-dynamic";
---
+++ app/api/human/user/route.js
@@ -0,0 +1,14 @@
+import {
+  getAllUser,
+  createUser,
+} from "@/modules/human/user/controllers/user.controller";
+
+export async function GET(request) {
+  return getAllUser(request);
+}
+
+export async function POST(request) {
+  return createUser(request);
+}
+
+export const dynamic = "force-dynamic";
