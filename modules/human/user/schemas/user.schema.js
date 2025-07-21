7d17b69a1c0d58638370551050cd7b227aab667b
Panuinc <panuwat.ja.forwork@gmail.com>
2025-07-21 11:31:26 +0700

    update

--- modules/human/user/schemas/user.schema.js
+++ modules/human/user/schemas/user.schema.js
@@ -3,23 +3,60 @@ import {
   preprocessInt,
   preprocessString,
   preprocessEnum,
+  preprocessDate,
+  preprocessFileFlexible,
   formatData,
 } from "@/lib/zodSchema";
 
 export const userPostSchema = z.object({
-  userFirstName: preprocessString("Please provide the user name"),
+  userFirstName: preprocessString("Please provide the user's first name"),
+  userLastName: preprocessString("Please provide the user's last name"),
+  userPhone: preprocessString("Please provide the user's phone"),
+  userEmail: preprocessString("Please provide the user's email"),
+  userPicture: preprocessFileFlexible(),
+
   userCreateBy: preprocessInt("Please provide the creator's user ID"),
+
+  useJobDivisionId: preprocessInt("Please provide Division ID"),
+  useJobDepartmentId: preprocessInt("Please provide Department ID"),
+  useJobPositionId: preprocessInt("Please provide Position ID"),
+  useJobRoleId: preprocessInt("Please provide Role ID"),
+  useJobStartDate: preprocessDate("Please provide start date (YYYY-MM-DD)"),
+  useJobContractType: preprocessEnum(
+    ["FullTime", "PartTime", "Internship", "Temporary", "Freelance"],
+    "Please provide contract type"
+  ),
 });
 
 export const userPutSchema = z.object({
-  userId: preprocessInt("Please provide the user ID to update"),
-  userFirstName: preprocessString("Please provide the user name"),
+  userId: preprocessInt("Please provide user ID"),
+  userFirstName: preprocessString("Please provide the user's first name"),
+  userLastName: preprocessString("Please provide the user's last name"),
+  userPhone: preprocessString("Please provide the user's phone"),
+  userEmail: preprocessString("Please provide the user's email"),
+  userPicture: preprocessFileFlexible(),
+
+  userUpdateBy: preprocessInt("Please provide the updater's user ID"),
+
+  useJobDivisionId: preprocessInt("Please provide Division ID"),
+  useJobDepartmentId: preprocessInt("Please provide Department ID"),
+  useJobPositionId: preprocessInt("Please provide Position ID"),
+  useJobRoleId: preprocessInt("Please provide Role ID"),
+  useJobStartDate: preprocessDate("Please provide start date (YYYY-MM-DD)"),
+  useJobEndDate: preprocessDate("Please provide start date (YYYY-MM-DD)"),
+  useJobContractType: preprocessEnum(
+    ["FullTime", "PartTime", "Internship", "Temporary", "Freelance"],
+    "Please provide contract type"
+  ),
   userStatus: preprocessEnum(
     ["Enable", "Disable"],
-    "Please provide 'Enable' or 'Disable'"
+    "Please provide 'Enable' or 'Disable"
   ),
-  userUpdateBy: preprocessInt("Please provide the updater's user ID"),
 });
 
 export const formatUserData = (users) =>
-  formatData(users, [], ["userCreateAt", "userUpdateAt"]);
+  formatData(
+    users,
+    ["useJobStartDate", "useJobEndDate"],
+    ["userCreateAt", "userUpdateAt"]
+  );
