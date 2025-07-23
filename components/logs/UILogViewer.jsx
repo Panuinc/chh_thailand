7adad413caadfb3652385d5e7c8bb21e482e6658
Panuinc <panuwat.ja.forwork@gmail.com>
2025-07-23 08:41:47 +0700

    update

--- components/logs/UILogViewer.jsx
+++ components/logs/UILogViewer.jsx
@@ -68,7 +68,7 @@ export default function UILogViewer({
               {selected ? (
                 <>
                   <div className="flex flex-row items-center justify-center w-full p-2 gap-2">
-                    <div className="flex items-center justify-start w-full h-full p-2 gap-2">
+                    <div className="flex items-center justify-start w-full h-full p-2 gap-2 font-semibold">
                       <Eye />
                       {selected.fileName}
                     </div>
