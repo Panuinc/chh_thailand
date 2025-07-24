09cd8f6e7e89739d0faec499977f27a2e481406a
Panuinc <panuwat.ja.forwork@gmail.com>
2025-07-24 11:21:14 +0700

    update

---
+++ app/(pages)/(sale)/layout.jsx
@@ -0,0 +1,5 @@
+import SideBarSection from "@/components/layout/SideBarSection";
+
+export default function SaleLayout({ children }) {
+  return <SideBarSection sectionKey="sale">{children}</SideBarSection>;
+}
---
+++ app/(pages)/(sale)/sale/page.jsx
@@ -0,0 +1,10 @@
+import UISale from "@/modules/sale/sale/components/UISale";
+import React from "react";
+
+export default function Sale() {
+  return (
+    <>
+      <UISale headerContent="Sale Dashboard"/>
+    </>
+  );
+}
