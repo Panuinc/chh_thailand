aeb5a81e5fe7382b64ba18d598bfb14b1891a4fc
Panuinc <panuwat.ja.forwork@gmail.com>
2025-08-01 09:46:36 +0700

    update

--- modules/warehouse/store/useCases/getAllStore.usecase.js
+++
@@ -1,9 +0,0 @@
-import { StoreService } from "../services/store.service";
-
-export async function GetAllStoreUseCase(page = 1, limit = 1000000) {
-  const skip = (page - 1) * limit;
-  const stores = await StoreService.getAllPaginated(skip, limit);
-  const total = await StoreService.countAll();
-
-  return { stores, total };
-}
--- modules/warehouse/store/validators/store.validator.js
+++
@@ -1,8 +0,0 @@
-import { StoreRepository } from "../repositories/store.repository";
-
-export const StoreValidator = {
-  async isDuplicateStoreCode(storeCode) {
-    const store = await StoreRepository.findByCode(storeCode);
-    return !!store;
-  },
-};
